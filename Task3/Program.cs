bf727781bd3a33705f5e490da8b90586e9c8ed06
Victoria <victoriaerygina@yandex.ru>
2022-08-19 13:57:30 +0700

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+switch (number)
+{
+    case 1: Console.WriteLine("Понедельник"); break;
+    case 2: Console.WriteLine("Вторник"); break;
+    case 3: Console.WriteLine("Среда"); break;
+    case 4: Console.WriteLine("Четверг"); break;
+    case 5: Console.WriteLine("Пятница"); break;
+    case 6: Console.WriteLine("Суббота"); break;
+    case 7: Console.WriteLine("Воскресенье"); break;
+    default: Console.WriteLine("ERROR"); break;
+}
+Console.ReadKey();
