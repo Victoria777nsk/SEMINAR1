77119e5a1190df2fbe701ad8f2e76adce86a0783
Victoria <victoriaerygina@yandex.ru>
2022-08-19 14:21:46 +0700

    Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = -number;
+
+while (number2 <= number)
+{
+    Console.Write($"{number2}, "); //Console.Write(number2+", ");
+    number2++;
+}
