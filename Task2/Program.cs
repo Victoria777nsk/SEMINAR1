89988bf3097e45b494aa8b502eb96f4377f5f9d1
Victoria <victoriaerygina@yandex.ru>
2022-08-19 12:29:26 +0700

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите квадрат числа: ");
+int Квадрат_Числа = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int Число = Convert.ToInt32(Console.ReadLine());
+
+
+if (Квадрат_Числа == (Число * Число))
+{
+Console.WriteLine("Да");
+}
+
+else
+{
+Console.WriteLine("Нет");
+}
