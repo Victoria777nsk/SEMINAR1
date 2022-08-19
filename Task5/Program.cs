46aca191eb84e56f00fab115ca634f243066e4e9
Victoria <victoriaerygina@yandex.ru>
2022-08-19 14:53:16 +0700

    Task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastNumber = number % 10;
+
+if ((number >= 100) && (number < 1000))
+{
+    Console.WriteLine("Последняя цифра: " + lastNumber);
+}
+else
+{
+    Console.WriteLine("ERROR");
+}
